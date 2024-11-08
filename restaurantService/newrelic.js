a4b882f0d5f8618aaa16247e517ed57b56d4113c
jojodasari <160972293+jojodasari@users.noreply.github.com>
2024-11-08 13:23:17 -0500

    Update newrelic.js

--- restaurantService/newrelic.js
+++ restaurantService/newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'NRAK-SIHGJN4RCPZEHKFCY1SVTX71IUQ',
+  license_key: '877c1acde5f1442ca57483cb90928882FFFFNRAL',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
